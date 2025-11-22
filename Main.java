7d65bff9145740e6997175b2f65be653730859d7
raihacks <siddhirai326@gmail.com>
2025-11-22 23:22:00 +0530

    java

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main{
+    public static void main(String[] args){
+        // System.out.println("heelo world");
+        // int age=1;
+        // int year = 2025;
+        // System.out.println(year);  
+        
+    }
+}
