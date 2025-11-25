a34524de8b61e4d8bd2b838dfd14d0af992899a4
raihacks <siddhirai326@gmail.com>
2025-11-25 15:20:01 +0530

    3:20

--- Main.java
+++ Main.java
@@ -190,6 +190,13 @@ import java.util.Scanner;
 
 public class Main{
     public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Welcome to Weight Conversion Program");
+        double weight;
+        double newWeight;
+        int choice;
+        System.out.println("");
+
 
     }
 }
