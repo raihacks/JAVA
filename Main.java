b03a3ddf62e5e956ca53c0413c8098fca73a3ff2
raihacks <siddhirai326@gmail.com>
2025-11-25 13:22:20 +0530

    email

--- Main.java
+++ Main.java
@@ -167,9 +167,29 @@ import java.util.Scanner;
 //     }
 // }
 
+// public class Main{
+//     public static void main(String[] args){
+//         Scanner sc = new Scanner(System.in);
+
+//         String email;
+//         String username;
+//         String domain;
+//         System.out.print("Enter your email: ");
+//         email = sc.nextLine();
+//         if(email.contains("@")){
+//             username = email.substring(0, email.indexOf("@"));
+//             domain = email.substring(email.indexOf("@")+1);
+//             System.out.println(username);
+//             System.out.println(domain);
+//         }
+//         else{
+//             System.out.println("enter the correct email id.");
+//         }
+//     }
+// }
+
 public class Main{
     public static void main(String[] args){
-
 
     }
 }
