f40b1084de415fb144c4190b1124c5fbca497934
raihacks <siddhirai326@gmail.com>
2025-11-26 11:02:35 +0530

    swicth

--- Main.java
+++ Main.java
@@ -258,7 +258,36 @@ import java.util.Scanner;
         // }
 //}
 
+public class Main{
+    public static void main(String[] args){
+        String day = "Monday";
 
+        if(day.equals("Monday")){
+            System.out.println("It is a weekday");
+        }
+        else if(day.equals("Tuesday")){
+            System.out.println("It is a weekday");
+        }
+        else if(day.equals("Wednesday")){
+            System.out.println("It is a weekday");
+        }
+        else if(day.equals("Thursday")){
+            System.out.println("It is a weekday");
+        }
+        else if(day.equals("Firday")){
+            System.out.println("It is a weekday");
+        }
+        else if(day.equals("Saterday")){
+            System.out.println("It is a NOT weekend");
+        }
+        else if(day.equals("Sunday")){
+            System.out.println("It is a NOT weekend");
+        }
+        else{
+            System.out.println("Enter the right day");
+        }
+    }
+}
 
 // public class Main{
 //     public static void main(String[] args){
