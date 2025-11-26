f784594dd48564f1ee258678f8ba6eb2661a11ef
raihacks <siddhirai326@gmail.com>
2025-11-26 11:39:48 +0530

    new file

--- Main.java
+++ Main.java
@@ -313,6 +313,6 @@ public class Main{
 
 // public class Main{
 //     public static void main(String[] args){
-
+        
 //     }
 // }
---
+++ Main1.java
@@ -0,0 +1,5 @@
+public class Main1{
+    public static void main(String[] args){
+        Scanner sc = new Scanner 
+    }
+}
