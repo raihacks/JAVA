e730f000638fc324e85ae1f3f8d8ffd0a684428e
raihacks <siddhirai326@gmail.com>
2025-11-26 09:00:46 +0530

    wright converter

--- Main.java
+++ Main.java
@@ -195,9 +195,25 @@ public class Main{
         double weight;
         double newWeight;
         int choice;
-        System.out.println("");
-
-
+        System.out.println("1: Convert lbs to kgs");
+        System.out.println("2:Convert kgs to lbs");
+        System.out.println("Chosse an option: ");
+        choice = sc.nextInt();
+        if(choice==1){
+            System.out.println("Enter weight in kgs:");
+            weight = sc.nextDouble();
+            newWeight = weight * 2.20462;
+            System.out.printf("the weight in lbs is: %.2f", newWeight);
+        }
+        else if(choice==2){
+            System.out.println("Enter weight in lbs:");
+            weight = sc.nextDouble();
+            newWeight = weight * 0.453592;
+            System.out.printf("the weight in kgs is: %.2f\n", newWeight);
+        }
+        else{
+            System.out.println("Enter the right choice.");
+        }
     }
 }
 
