bdf6408dab81f518e14e2efc1b262d5e19786f40
raihacks <siddhirai326@gmail.com>
2025-11-27 09:35:43 +0530

    partten

--- Main1.java
+++ Main1.java
@@ -133,6 +133,32 @@ import java.util.Random;
 //     }
 // }
 
+// public class Main1{
+//     public static void main(String[] args){
+        // for(int i = 1; i <= 3; i++){
+        //     for(int j = 1; j <= 9; j++){
+        //         System.out.print(j + " ");
+        //     }
+        // }
+//         Scanner sc = new Scanner(System.in);
+//         int rows;
+//         int columns;
+//         char symbol;
+//         System.out.println("Enter the # of rows: ");
+//         rows = sc.nextInt();
+//         System.out.println("Enter the # of columns: ");
+//         columns = sc.nextInt();
+//         System.out.println("Enter the symbol to be printed: ");
+//         symbol = sc.next().charAt(0);
+//         for(int i = 0;i < rows; i++){
+//             for(int j = 0;j < columns; j++){
+//                 System.out.print(symbol);
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
+
 public class Main1{
     public static void main(String[] args){
         
