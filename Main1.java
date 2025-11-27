53ba5a16f2e92bfba8eeb3d88e098fe3625e5bdf
raihacks <siddhirai326@gmail.com>
2025-11-27 09:07:17 +0530

    contuine break

--- Main1.java
+++ Main1.java
@@ -121,6 +121,18 @@ import java.util.Random;
 //     }
 // }
 
+// public class Main1{
+//     public static void main(String[] args){
+//         for(int i = 0; i <10; i++){
+//             if(i == 5){
+//                 //break;
+//                 continue;
+//             }
+//             System.out.print(i + " ");
+//         }
+//     }
+// }
+
 public class Main1{
     public static void main(String[] args){
         
