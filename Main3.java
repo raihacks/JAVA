3fb0e311487cd1c27ff99f6e4a4337d85bb0f4a6
raihacks <siddhirai326@gmail.com>
2025-11-29 10:31:18 +0530

    rock paper scissor

--- Main3.java
+++ Main3.java
@@ -1,4 +1,37 @@
 import java.util.Scanner;
+import java.util.Random;
+// public class Main3{
+//     public static void main(String[] args){
+//         Scanner sc = new Scanner(System.in);
+//         Random rn = new Random();
+//         String[] choice ={"rock", "paper", "scissors"};
+//         String compChoice;
+//         String playAgain = "yes";
+//         do{
+//         System.out.println("Chosee between (rock/paper/scissors): ");
+//         String playerChoice= sc.nextLine().toLowerCase();
+//         if(!playerChoice.equals("rock") && !playerChoice.equals("paper") && !playerChoice.equals("scissors")){
+//             System.out.println("Invalid choice");
+//             continue;
+//         }
+//         compChoice = choice[rn.nextInt(3)];
+//         System.out.println("Computer Choice: "+compChoice);
+//         if(playerChoice.equals(compChoice)){
+//             System.out.println("It is a tie.");
+//         }
+//         else if((playerChoice.equals("rock") && compChoice.equals("scissors")) || (playerChoice.equals("scissors") && compChoice.equals("paper")) || (playerChoice.equals("paper") && compChoice.equals("rock"))){
+//             System.out.println("U win.");
+//         }
+//         else{
+//             System.out.println("U lose.");
+//         }
+//         System.out.println("Play again(Yes/no): ");
+//         playAgain = sc.nextLine().toLowerCase();
+//         }while(playAgain.equals("yes"));
+//         System.out.println("Thank you for playing");
+//     }
+// }
+
 public class Main3{
     public static void main(String[] args){
         System.out.println(" ");
