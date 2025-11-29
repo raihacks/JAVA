7df4c68e15f7c54decb144669f3048da2728c55a
raihacks <siddhirai326@gmail.com>
2025-11-29 08:37:27 +0530

    tele using 2D

--- Main2.java
+++ Main2.java
@@ -264,7 +264,26 @@
 //     }
 // }
 
+// public class Main2{
+//     public static void main(String[] args){
+//         char tele[][]={ {'1', '2', '3'},
+//                         {'4', '5', '6'},
+//                         {'7', '8', '9'},
+//                         {'*', '0', '#'}};
+//         for(char[] number:tele){
+//             for(char nums:number){
+//               System.out.print(nums + "  ");  
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
 
+public class Main2{
+    public static void main(String[] args){
+        System.out.println(" ");
+    }
+}
 
 // public class Main2{
 //     public static void main(String[] args){
