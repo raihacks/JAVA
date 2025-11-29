ab8e810bcfa7ae8a8d5aae0ff1e417d548f69106
raihacks <siddhirai326@gmail.com>
2025-11-29 16:11:01 +0530

    a

--- Main3.java
+++ Main3.java
@@ -114,6 +114,12 @@ import java.util.Random;
 //     }
 // }
 
+public class Main3{
+    public static void main(String[] args){
+        System.out.println(" ");
+    }
+}
+
 // public class Main3{
 //     public static void main(String[] args){
 //         System.out.println(" ");
