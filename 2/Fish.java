bdddefc38aeaee04bb54ac22e790e9bf796e8674
raihacks <siddhirai326@gmail.com>
2025-11-30 21:48:31 +0530

    interface

---
+++ 2/Fish.java
@@ -0,0 +1,6 @@
+public class Fish implements Prey{
+    @Override
+    public void flee(){
+        System.out.println("*The Fish is siwwing away*");
+    }
+}
--- Main.java
+++ Main.java
@@ -40,3 +40,14 @@
 //         System.out.println(rectangle.area());
 //     }
 // }
+
+public class Main{
+    public static void main(String[] args){
+        Rabbit rabbit = new Rabbit();
+        Fish fish = new Fish();
+        Hawk hawk = new Hawk();
+        rabbit.flee();
+        fish.flee();
+        hawk.hunt();
+    }
+}
