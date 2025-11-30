1856b3a6e43e3789b39e2e060fabec8825c0b2f2
raihacks <siddhirai326@gmail.com>
2025-11-30 17:40:14 +0530

    method overriding

--- Main.java
+++ Main.java
@@ -9,14 +9,15 @@
 //     }
 // }
 
-public class Main{
-    public static void main(String[] args){
-        Dog dog = new Dog();
-        Cat cat = new Cat();
-        Fish fish = new Fish();
+// public class Main{
+//     public static void main(String[] args){
+//         Dog dog = new Dog();
+//         Cat cat = new Cat();
+//         Fish fish = new Fish();
+//         dog.move();
+//         cat.move();
+//         fish.move();
+//     }
+// }
 
-        dog.move();
-        cat.move();
-        fish.swim();
-    }
-}
+
---
+++ inq/Fish.java
@@ -0,0 +1,5 @@
+public class Fish extends Animal{
+    void move(){
+        System.out.println("This is animal is swimming.");
+    }
+}
