ea853176792b62602b7009f9c962de3e68d9d6ad
raihacks <siddhirai326@gmail.com>
2025-12-02 09:25:51 +0530

    a

---
+++ kunal kushwaha/Main.java
@@ -0,0 +1,14 @@
+// public class Main{
+//     public static void main(Strings[] args){
+//         Student[] student = new Student[];
+//         Student[] student1 = new Student(12 ,"anya" ,);
+//         Student[] student2= new Student();
+//         Student[] student3 = new Student();
+//         Student[] student4 = new Student();
+//     }
+//     class Student{
+//         int[] rollno = new int[5];
+//         String[] name = new String[5];
+//         float[] marks = new float[5];
+//     }  
+// }
