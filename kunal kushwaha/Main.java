3b38ad82374a95ea5822aba5f37b9168a00e9014
raihacks <siddhirai326@gmail.com>
2025-12-06 11:29:45 +0530

    command line argument

--- kunal kushwaha/Main.java
+++ kunal kushwaha/Main.java
@@ -1,24 +1,31 @@
+// public class Main{
+//     public static void main(String[] args){
+//         Student student = new Student(12, "Anya", 123.60f);
+//         System.out.println(student);
+//     } 
+// }
+// class Student{
+//     int rollno;
+//     String name;
+//     float marks;
+//     Student(int rollno, String name, float marks){
+//         this.rollno = rollno;
+//         this.name = name;
+//         this.marks = marks;
+//     }
+//     @Override
+//     public String toString(){
+//         return "Student { rollno = " + rollno +
+//                ", name = '" + name + '\'' +
+//                ", marks = " + marks +
+//                " }";
+//     }
+// }
+
 public class Main{
     public static void main(String[] args){
-        Student student = new Student(12, "Anya", 123f);
-        System.out.println(student);
-    } 
+        System.out.println(args[0]);
+        System.out.println(args[1]);
+        System.out.println(args[2]);
+    }
 }
-class Student{
-    int rollno;
-    String name;
-    float marks;
-    Student(int rollno, String name, float marks){
-        this.rollno = rollno;
-        this.name = name;
-        this.marks = marks;
-    }
-    @Override
-    public String toString(){
-        return "Student { rollno = " + rollno +
-               ", name = '" + name + '\'' +
-               ", marks = " + marks +
-               " }";
-    }
-} 
-
