e5a3edc47bf08678457b3735ad8d2984902f74a0
raihacks <siddhirai326@gmail.com>
2025-12-08 08:03:46 +0530

    end

--- kunal kushwaha/Main.java
+++ kunal kushwaha/Main.java
@@ -22,10 +22,16 @@
 //     }
 // }
 
-public class Main{
-    public static void main(String[] args){
-        System.out.println(args[0]);
-        System.out.println(args[1]);
-        System.out.println(args[2]);
-    }
-}
+// public class Main{
+//     public static void main(String[] args){
+//         System.out.println(args[0]);
+//         System.out.println(args[1]);
+//         System.out.println(args[2]);
+//     }
+// }
+
+// public class Main{
+//     public static void main(String[] args){
+        
+//     }
+// }
